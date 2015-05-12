ff95a47700cac2e8f4318b539806e9bc538c3b8a
Niels Braczek <nbraczek@bsds.de>
2015-05-12 13:42:47 +0200

    Added reverse proxy to serve multiple domains per container

---
+++ build/docker/nginx-proxy/docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+if [ "${1:0:1}" = '-' ]; then
+	set -- nginx "$@"
+fi
+
+if [ "$1" = 'nginx' ]; then
+    echo "Apache: ${WEB_APACHE_PORT_80_TCP_ADDR}"
+    echo "Nginx:  ${WEB_NGINX_PORT_80_TCP_ADDR}"
+    sleep 1
+    echo "Apache: ${WEB_APACHE_PORT_80_TCP_ADDR}"
+    echo "Nginx:  ${WEB_NGINX_PORT_80_TCP_ADDR}"
+    sleep 1
+    echo "Apache: ${WEB_APACHE_PORT_80_TCP_ADDR}"
+    echo "Nginx:  ${WEB_NGINX_PORT_80_TCP_ADDR}"
+    sleep 1
+    echo "Apache: ${WEB_APACHE_PORT_80_TCP_ADDR}"
+    echo "Nginx:  ${WEB_NGINX_PORT_80_TCP_ADDR}"
+    sleep 1
+    echo "Apache: ${WEB_APACHE_PORT_80_TCP_ADDR}"
+    echo "Nginx:  ${WEB_NGINX_PORT_80_TCP_ADDR}"
+    sleep 1
+    echo "Setting IP address of Apache container"
+    sed -i "s/APACHE_IP/${WEB_APACHE_PORT_80_TCP_ADDR}/" /etc/nginx/conf.d/*
+    echo "Setting IP address of Nginx container"
+    sed -i "s/NGINX_IP/${WEB_NGINX_PORT_80_TCP_ADDR}/" /etc/nginx/conf.d/*
+fi
+
+exec "$@"

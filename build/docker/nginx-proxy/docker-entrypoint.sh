ca324059d8d8067db77bc19235b50e56c808b48a
Niels Braczek <nbraczek@bsds.de>
2015-05-13 00:20:42 +0200

    Reached milestone 4: nginx + php-fpm works

--- build/docker/nginx-proxy/docker-entrypoint.sh
+++ build/docker/nginx-proxy/docker-entrypoint.sh
@@ -6,24 +6,10 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 if [ "$1" = 'nginx' ]; then
-    echo "Apache: ${WEB_APACHE_PORT_80_TCP_ADDR}"
-    echo "Nginx:  ${WEB_NGINX_PORT_80_TCP_ADDR}"
-    sleep 1
-    echo "Apache: ${WEB_APACHE_PORT_80_TCP_ADDR}"
-    echo "Nginx:  ${WEB_NGINX_PORT_80_TCP_ADDR}"
-    sleep 1
-    echo "Apache: ${WEB_APACHE_PORT_80_TCP_ADDR}"
-    echo "Nginx:  ${WEB_NGINX_PORT_80_TCP_ADDR}"
-    sleep 1
-    echo "Apache: ${WEB_APACHE_PORT_80_TCP_ADDR}"
-    echo "Nginx:  ${WEB_NGINX_PORT_80_TCP_ADDR}"
-    sleep 1
-    echo "Apache: ${WEB_APACHE_PORT_80_TCP_ADDR}"
-    echo "Nginx:  ${WEB_NGINX_PORT_80_TCP_ADDR}"
-    sleep 1
-    echo "Setting IP address of Apache container"
+
+    echo "Setting IP address of Apache container (${WEB_APACHE_PORT_80_TCP_ADDR})"
     sed -i "s/APACHE_IP/${WEB_APACHE_PORT_80_TCP_ADDR}/" /etc/nginx/conf.d/*
-    echo "Setting IP address of Nginx container"
+    echo "Setting IP address of Nginx container (${WEB_NGINX_PORT_80_TCP_ADDR})"
     sed -i "s/NGINX_IP/${WEB_NGINX_PORT_80_TCP_ADDR}/" /etc/nginx/conf.d/*
 fi
 

5bc41ef8cf530cf6aa6b27134ccd779663ac9cef
Niels Braczek <nbraczek@bsds.de>
2015-05-18 17:03:20 +0200

    Use symbolic names instead of (variable) IP addresses

--- build/docker/nginx-proxy/docker-entrypoint.sh
+++ build/docker/nginx-proxy/docker-entrypoint.sh
@@ -5,12 +5,4 @@ if [ "${1:0:1}" = '-' ]; then
 	set -- nginx "$@"
 fi
 
-if [ "$1" = 'nginx' ]; then
-
-    echo "Setting IP address of Apache container (${WEB_APACHE_PORT_80_TCP_ADDR})"
-    sed -i "s/APACHE_IP/${WEB_APACHE_PORT_80_TCP_ADDR}/" /etc/nginx/conf.d/*
-    echo "Setting IP address of Nginx container (${WEB_NGINX_PORT_80_TCP_ADDR})"
-    sed -i "s/NGINX_IP/${WEB_NGINX_PORT_80_TCP_ADDR}/" /etc/nginx/conf.d/*
-fi
-
 exec "$@"

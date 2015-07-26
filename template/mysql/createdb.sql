ee6f0cbcd9c1fb0a501c5d2d2b4787172461f216
Niels Braczek <nbraczek@bsds.de>
2015-07-26 12:47:38 +0200

    Minor cleanup

--- template/mysql/createdb.sql
+++ template/mysql/createdb.sql
@@ -2,5 +2,3 @@
 CREATE DATABASE IF NOT EXISTS `${environment.database.name}` CHARACTER SET 'utf8';
 GRANT ALL ON `${environment.database.name}`.* TO '${database.mysql.user}'@'%';
 USE `${environment.database.name}`;
-
-
